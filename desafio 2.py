724c586ddf2e4042597a39af1a8a1fb8cc77c498
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2020-08-13 17:04:26 -0300

    Add files via upload

---
+++ desafio 2.py
@@ -0,0 +1,10 @@
+nome = input('Digite seu nome:')
+dia = input('Digite dia em que você nasceu:')
+mes = input('Digite mês:')
+ano = input('Digite ano:')
+
+
+print('Nome:', nome)
+print('Nascimento:',dia,'/',mes,'/',ano)
+
+
