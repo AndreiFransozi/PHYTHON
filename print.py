b352208fb41b7b3885d93b2b67fd1284e7b3e2d8
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2020-08-13 17:29:32 -0300

    Add files via upload

---
+++ print.py
@@ -0,0 +1,46 @@
+x = 0
+while x < 10:
+    print(x)
+    x += 1
+    if x == 6:
+print("O céu resplandece ao meu redor (e ao meu redor)")
+print("Vou voar, e estrelas brilham entre as nuvens sem fim")
+print("Só a verdade vai cruzar pelo céu azul (pelo céu azul)")
+print("E a verdade vai crescer dentro de mim")
+
+print("Como um vulcão que entra em erupção")
+print("Sua lava vai espalhar")
+print("Verá toda a fúria do dragão")
+
+print("Cha-la head-cha-la")
+print("Não importa o que aconteça")
+print("Sempre tenho a força e o poder")
+print("Cha-la head-cha-la")
+print("Não importa o que aconteça")
+print("Tudo vai ficar melhor")
+
+print("O céu resplandece ao meu redor (e ao meu redor)")
+print("Vou voar, e estrelas brilham entre as nuvens sem fim")
+print("Só a verdade vai cruzar pelo céu azul (pelo céu azul)")
+print("E a verdade vai crescer dentro de mim")
+
+print("Como um vulcão que entra em erupção")
+print("Sua lava vai espalhar")
+print("Verá toda a fúria do dragão")
+
+print("Cha-la head-cha-la")
+print("Não importa o que aconteça")
+print("Sempre tenho a força e o poder")
+print("Cha-la head-cha-la")
+print("Vibrante o meu coração")
+print("Sente emoção e tem amor pra dar")
+
+print("Cha-la head-cha-la")
+print("Não pense em nada, só escuta")
+print("Sonhos dentro do coração")
+print("Cha-la head-cha-la")
+print("Não importa o que aconteça")
+print("Tudo vai ficar melhor")
+break
+else:
+    print("Toriama")
---
+++ sa.py
@@ -0,0 +1,9 @@
+x = 0
+while x < 40:
+    print(x)
+    x += 1
+    if x == 40:
+        print("x é igual a 6")
+        break
+else:
+    print("fim while")
